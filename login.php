e2cd938d6ded614746dc0aeb4ac4bd09ec03bed1
pranav <pmk3796@gmail.com>
2017-03-27 23:14:04 -0400

    reuite_once

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
-<?php $fb = new Facebook\Facebook([
+<?php 
+require_once __DIR__ . '/vendor/autoload.php';
+
+$fb = new Facebook\Facebook([
   'app_id' => '122358084967357', // Replace {app-id} with your app id
   'app_secret' => '9bd803dc1de9df2850e2d935beed9d34',
   'default_graph_version' => 'v2.2',
